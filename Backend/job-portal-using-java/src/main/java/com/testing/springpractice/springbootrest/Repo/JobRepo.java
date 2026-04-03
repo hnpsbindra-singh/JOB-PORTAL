528c08622ee9526e9fe24555089ac7eeca7856d0
Harnimarpreet Singh Bindra <hnps.bindra@gmail.com>
2026-04-04 03:34:06 +0530

    Update JobRepo.java

--- Backend/job-portal-using-java/src/main/java/com/testing/springpractice/springbootrest/Repo/JobRepo.java
+++ Backend/job-portal-using-java/src/main/java/com/testing/springpractice/springbootrest/Repo/JobRepo.java
@@ -4,8 +4,6 @@ import com.testing.springpractice.springbootrest.Model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -13,4 +11,5 @@ public interface JobRepo extends JpaRepository<JobPost, Integer> {
 
     List<JobPost> findByPostProfileContainingOrPostDescContaining(String Keyword1, String keyword2);
 
+    List<JobPost> findTop5ByOrderByPostIdDesc();
 }
